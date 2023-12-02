f8a863656bc9db13ef9c8c6195c61c67489b0a89
kip <himself@dennizvd.com>
2023-12-02 18:04:38 +0100

    Update script.js

--- f16/script.js
+++ f16/script.js
@@ -1,3 +1,39 @@
+/* localStorage */
+set_font_size = function(size) {
+	localStorage.getItem("font_size");
+  data = localStorage.setItem('font_size', size);
+}
+set_all_extra_info = function(extraInfoVal) {
+	localStorage.getItem("all_extra_info");
+  data = localStorage.setItem('all_extra_info', extraInfoVal);
+}
+// Retrieve
+var fontSize = localStorage.getItem('font_size');
+var allExtraInfo = localStorage.getItem('all_extra_info');
+if(fontSize == null) {
+  set_font_size(16);
+} else {
+  $("#textSlider").val(fontSize);
+  $('body').css("font-size", fontSize + "px");
+  $('#fontSize').text(fontSize);
+}
+if(allExtraInfo == null) {
+  set_all_extra_info(true);
+  $('#info-all').prop('checked', true);
+} else {
+  if(allExtraInfo == 'true') {
+    $('#info-all').prop('checked', true);
+  } else {
+    $('#info-all').trigger('change');
+    $('.all-others').prop('checked', false);
+    $('.fullinfo').css({"display": "none"});
+  }
+}
+/*alert(allExtraInfo);
+document.getElementById("test").innerHTML = 
+localStorage.getItem("all_extra_info");*/
+/* localStorage */
+
 /* Settings */
 /* Fullscreen */
 var elem = document.documentElement;
@@ -23,6 +59,7 @@ function closeFullscreen() {
 $("#textSlider").on("input",function () {
 	$('body').css("font-size", $(this).val() + "px");
 	$('#fontSize').text($(this).val());
+  set_font_size($(this).val());
 });
 /* Settings */
 
@@ -43,32 +80,16 @@ $('#info-all').change(function() {
     var $this = $(this);
   	if ($this.prop('checked')) {
     	$('.all-others').prop('checked', true);
+      set_all_extra_info(true);
 		$('.fullinfo').css({"display": "block"});
   	} else {
-		$('.all-others').prop('checked', false);
-		$('.fullinfo').css({"display": "none"});
-	}
+		  $('.all-others').prop('checked', false);
+		  $('.fullinfo').css({"display": "none"});
+      set_all_extra_info(false);
+	  }
 });
 $('.all-others').change(function() {
-    var getId = $(this).attr('id'); 
+  var getId = $(this).attr('id'); 
 	$('#'+getId+'div').toggle();
 });
 /* Checkboxes */
-
-/* localStorage */
-var data = localStorage.getItem('my_data');
-set_data = function() {
-	localStorage.getItem("my_data");
-  data = localStorage.setItem('my_data', 'whatever');
-  // for testing
-  alert(localStorage.getItem('my_data'));
-}
-delete_data = function() {
-	data = localStorage.clear();
-  // for testing
-  alert(localStorage.getItem('my_data'));
-}
-// Retrieve
-document.getElementById("demo").innerHTML = 
-localStorage.getItem("my_data");
-/* localStorage */
